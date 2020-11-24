c9ea921eae5efa35217606073804f4d9965dd8cf
ChoiceABC <nz09098488@gmail.com>
2020-11-23 19:07:32 -0500

    1

--- Compare.py
+++ Compare.py
@@ -6,7 +6,6 @@ def compare(a,b):
         a1stBox=b
         a2ndBox=a
     # print(f'{a1stBox=},{a2ndBox=}')
-    
-            
+       
 if __name__ == "__main__":
     compare(int(input("Please type the number you want to compare: ")),int(input("Please type the second number you want to compare: ")))
