2ac2b24637042739a0c2a06a6959f14cdf48067e
ChoiceABC <nz09098488@gmail.com>
2020-11-26 20:36:46 -0500

    mode

--- string-list-tuple-set-map-dict-2.py
+++ string-list-tuple-set-map-dict-2.py
@@ -1,7 +1,8 @@
+#%%
 print("\n------- string ----------\n")
 str0 = ''                   # create ---> C
-str1 = " I am learning Python! "
-str2 = "Python is awsome!"
+str1 = "Pythann"
+str2 = 'Pythan awesome!'
 print(str1+str2)
 print(2*str1)
 print(str2*2)
@@ -14,17 +15,19 @@ print(str1[0:-1])
 print(str1[0:])
 print(str1[:-1])
 
+# str1[1]="w"  # uchangeable ----> U
+
 print(f'{str1=}')
 # del str1[1]           #delete --> D  but the string is uchangeable
 print(f'{str1=}')
 
-# str1[1]="w"  # uchangeable ----> U
 
+#%%
 print("\n------- tuple ----------\n")
 str0 = ()
 str9 = (1,)
-str1 = tuple(" I am learning Python! ")
-str2 = tuple("Python is awsome!")
+str1 = tuple("Pythann")
+str2 = tuple("Pythan awesome!")
 print(str1+str2)
 print(2*str1)
 print(str2*2)
@@ -36,22 +39,25 @@ print(str1[4:-1])
 print(str1[0:-1])
 print(str1[0:])
 print(str1[:-1])
+
+# str1[1]="w"  # uchangeable
 
 print(f'{str1=}')
 # del str1[1]           #delete --> D  but the string is uchangeable
 print(f'{str1=}')
 
-# str1[1]="w"  # uchangeable
 
+#%%
 print("\n------- list ----------\n")
 str0 = []
-str1 = list(" I am learning Python! ")
-str2 = list("Python is awsome!")
+str1 = list("Pythonn")
+str2 = list("Python awesome!")
 print(str1+str2)
 print(2*str1)
 print(str2*2)
 
 print(str1[4])
+print(str1[4:6])
 print(str1[4:-1:2])
 print(str1[4:-1:1])
 print(str1[4:-1])
@@ -59,28 +65,32 @@ print(str1[0:-1])
 print(str1[0:])
 print(str1[:-1])
 print(f'{str1=}')
-str1[1] = "w"  # can be change --->U
+str1[1] = "w"  # can be change(update) --->U
 print(f'{str1=}')
 del str1[1]    #delete --> D
 print(f'{str1=}')
 
+
+#%%
 print("\n------- set ----------\n")
 str0 = set()
 str00 = {1, 2}
-str1 = set(" I am learning Python! ")
-str2 = set("Python is awsome!")
+str1 = set("Python!")
+str2 = set("Python awesome!")
 print(str1)
 print(str2)
-str1.add(100)
+str1.add(100) # can be change(updat) --->U
 print(str1)
-str1.remove(100)
+str1.remove(100) # can be delete --->D
 print(str1)
+print()
 print(f'{str1 - str2=}')
 print(f'{str1 | str2=}')
 print(f'{str1 & str2=}')
 print(f'{str1 ^ str2=}')
 
-print("\n------- dictionary ----------\n")
+#%%
+print("\n------- map/dictionary ----------\n")
 str0={}
 str1=dict([('apple',2),('pearl', 3),('orange',15)])
 str2=dict(apple=2,pearl=3,orange=15)
@@ -92,7 +102,12 @@ print(f'{str1=}')
 del str1['apple']  #delete --> D
 print(f'{str1=}')
 
+
+
+print("\n------- ASCII and ordinal ----------\n")
 print(ord('中'))
 print(ord('a'))
 print(chr(ord('中')))
 print(chr(ord('a')))
+
+# %%
