c031f0862e225f4d1593332a39d18890f883b2e4
ChoiceABC <nz09098488@gmail.com>
2020-11-27 21:51:17 -0500

    1

--- collection-list-practise.py
+++ collection-list-practise.py
@@ -1,6 +1,9 @@
 a=[]
 print(f'{a=} {type(a)=}')
 
+b=list()
+print(f'{b=} {type(b)=}')
+
 a=[3,]
 print(f'{a=} {type(a)=}')
 
@@ -9,3 +12,6 @@ print(f'{a=} {type(a)=}')
 
 del a
 print(f'{a=} {type(a)=}')
+
+# https://note.nkmk.me/en/python-list-clear-pop-remove-del/
+# https://note.nkmk.me/en/python-list-append-extend-insert/
--- collection-set-practice.py
+++ collection-set-practice.py
@@ -1,3 +1,4 @@
+#%%
 a=set()
 print(f'{a=} {type(a)=}')
 a={}
@@ -12,5 +13,14 @@ a.clear()
 print(f'{a=} {type(a)=}')
 a.add(5)
 print(f'{a=} {type(a)=}')
+
+#%%
+a={1,'3','f',"y",100,3.5}
+length=len(a)
+for _ in range(length):
+    a.pop()
+    print(a)
+
+#%%
 del a
 print(f'{a=} {type(a)=}')
