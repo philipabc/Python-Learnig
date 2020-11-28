8fceb389a4228b3ac4cc88860a5a7d622b8031cb
ChoiceABC <nz09098488@gmail.com>
2020-11-27 20:45:20 -0500

    1

--- CheckHowMany.py
+++ CheckHowMany.py
@@ -1,9 +1,10 @@
 count=0 #C
 map={}
 hi=[2,1,3,6,3,3,4,5,5,6,6,7,8,9,9,10,10]
+hi.append(int(input("Please type the number you want to add: ")))
 HI=int(input("Please type the number that you want to check in this list: "))
-print(hi)
-
+hi[(len(hi)-1)]=100
+# print(hi)
 # for i in range(len(hi)):
     # if HI==hi[i]:
 for i in hi:
@@ -11,7 +12,7 @@ for i in hi:
         
         count+=1
         map={HI:count}
-print("The number you typed showed",count,"times in this list")
+print("The number you typed showed",count,"time(s) in this list")
 keys=set(hi)
 for k in keys:
     count=0
@@ -26,3 +27,8 @@ for k in keys:
 # print(map)
 # map={a:1001}
 # print(map)
+# hi=tuple(hi)
+# print(hi)
+
+hi[20]=3
+print(hi)
