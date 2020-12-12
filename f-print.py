5e69947ac5efd8c37b6c2a2cd92f19570eb9a1ff
ChoiceABC <nz09098488@gmail.com>
2020-12-12 11:55:30 -0500

    1

--- f-print.py
+++ f-print.py
@@ -11,3 +11,23 @@ print(f'{num=:#x} {num=:#b} {num=:#o}')
 # width       ::=  integer
 # precision   ::=  integer
 # type        ::=  "b" | "c" | "d" | "e" | "E" | "f" | "F" | "g" | "G" | "n" | "o" | "s" | "x" | "X" | "%"
+
+#%%
+a=-1234567890.0947 # width=1+10+1+4, precision=4
+print(f'{a=}')
+print(f'{a=:@<+#015,.4f}')
+print(f'{a=:@<+#015,.1f}')
+print(f'{a=:@<+#20.4f}\n')
+
+print(f'{a=:@>+#25,.6f}')
+print(f'{a=:@>+#025,.6f}')
+print(f'{a=:@^+#025,.6f}')
+print(f'{a=:@=+#025,.6f}')
+print(f'{a=:@=#025,.6f}')
+print(f'{a=:@^#025,.6f}')
+print(f'{a=:@^#025,.6f}\n')
+
+print(f'{a=:^#025,.2f}')
+print(f'{a=:<#025,.2f}')
+print(f'{a=:>#025,.2f}')
+# %%
